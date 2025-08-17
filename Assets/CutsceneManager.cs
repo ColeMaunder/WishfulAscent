6da4f7de070e09e6b44fa8efc4aa900e5ea7aace
ColeMaunder <Cole@maunder.co.za>
2025-08-17 14:53:59 +0200

    Start screne and cutsene

---
+++ Assets/CutsceneManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public Sprite[] Scene1;
+    public SpriteRenderer diplay;
+    public int firstFrame = 0;
+    private int currentFrame;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        currentFrame = firstFrame;
+        diplay.sprite = Scene1[currentFrame];
+    }
+    public void nextFrame(){
+        currentFrame++;
+        if (Scene1.Length > currentFrame) {
+            currentFrame = 0;
+        }
+        diplay.sprite = Scene1[currentFrame];
+    }
+}
---
+++ Assets/CutseneTemp.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CutseneTemp : MonoBehaviour
+{
+    public Sprite[] Scene1;
+    public SpriteRenderer diplay;
+    public int firstFrame = 0;
+    private int currentFrame;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //currentFrame = firstFrame;
+        //diplay.sprite = Scene1[currentFrame];
+    }
+    public void nextFrame(){
+        currentFrame++;
+        if (Scene1.Length > currentFrame) {
+            currentFrame = 0;
+        }
+        diplay.sprite = Scene1[currentFrame];
+    }
+}
