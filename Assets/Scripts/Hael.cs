a6550fb94f3f888c38c8ee84853e0c6dd6c773ef
ColeMaunder <cole@maunder.co.za>
2025-08-22 16:03:48 +0200

    Orb Return 1.0

--- Assets/Scripts/Hael.cs
+++ Assets/Scripts/Hael.cs
@@ -18,8 +18,10 @@ public class Hael : MonoBehaviour
     private float fealdScale = 1f;
     public float fealdScaleBase = 1f;
     public float fealdScaleMax = 25f;
-    private Coroutine scaleUp,scaleDown;
+    private Coroutine scaleUp, scaleDown;
     Transform gravFeald;
+    bool orbReturning = false;
+    Coroutine orbReturnProsess;
     void Start()
     {
         controller = transform.parent.gameObject.GetComponent<FPController>();
@@ -28,85 +30,149 @@ public class Hael : MonoBehaviour
         orbBody = gravOrb.GetComponent<TimeForce>();
         gravFeald = gravOrb.transform.GetChild(0).GetComponent<Transform>();
     }
-    void Update()
+    void FixedUpdate()
     {
-        if(gravOrb.transform.parent != null && Vector3.Distance(gravOrb.transform.localPosition, orbHold) > dropVeriance){
+        if (gravOrb.transform.parent != null && Vector3.Distance(gravOrb.transform.localPosition, orbHold) > dropVeriance)
+        {
             gravOrb.transform.SetParent(null);
             orbBody.Sleep();
         }
     }
-    public void GravOrbControll(InputAction.CallbackContext context){
-        if(transform == controller.GetActiveCharicter()){
+    public void GravOrbControll(InputAction.CallbackContext context)
+    {
+        if (transform == controller.GetActiveCharicter())
+        {
             Rigidbody rb = gravOrb.transform.GetComponent<Rigidbody>();
-            if (context.performed) {
+            if (context.performed)
+            {
                 rb.constraints = RigidbodyConstraints.None;
-                if (gravOrb.transform.parent != null) {
+                if (gravOrb.transform.parent != null)
+                {
                     gravOrb.transform.SetParent(null);
                     orbBody.AddForce(camra.forward * trowForce);
                 } else {
+                    Vector3 orbHoldWorld = transform.TransformPoint(orbHold);
+                    Vector3 orbPosition = orbBody.transform.position;
+                    Vector3 direction = (orbHoldWorld - orbPosition).normalized;
+                    float distance = Vector3.Distance(orbPosition , orbHoldWorld);
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(orbPosition, direction, out hit, distance)  /*&& hit.collider.gameObject.GetComponent<TimeForce>() == null*/){
+                        Debug.Log("Orb return blocked by : " + hit.collider.gameObject.name);
+                        if (orbReturnProsess != null)
+                        {
+                            StopCoroutine(orbReturnProsess);
+                            orbReturning = false;
+                        }
+                        gravOrb.transform.SetParent(camra);
+                        gravOrb.transform.localPosition = orbHold;
+                    }else{
+                        orbReturnProsess = StartCoroutine(OrbBack(rb));
+                        Debug.Log("Orb Going to  : " + orbHoldWorld);
+                    }
+                    Debug.DrawRay(orbPosition, direction * distance, Color.red);
+
                     //Vector3 direction = Vector3.Normalize(camra.position - gravOrb.transform.position);
                     //orbBody.AddForce(direction  *  trowForce);
-                    gravOrb.transform.SetParent(camra);
-                    gravOrb.transform.localPosition = orbHold;
+
+                    //StartCoroutine(OrbBack(rb));
+                    //gravOrb.transform.SetParent(camra);
+                    //gravOrb.transform.localPosition = orbHold;
                 }
-            }else{
+            }
+            else
+            {
                 orbBody.Sleep();
                 rb.constraints = RigidbodyConstraints.FreezePosition;
             }
         }
-        
+
     }
-    public void ToggleActive(InputAction.CallbackContext context){
-        if(transform == controller.GetActiveCharicter()){
-            if(context.performed && !hasChecked){
+
+    public void ToggleActive(InputAction.CallbackContext context)
+    {
+        if (transform == controller.GetActiveCharicter())
+        {
+            if (context.performed && !hasChecked)
+            {
                 holdLocked = !holdLocked;
-                if(!holding){
-                    if(holdLocked){
+                if (!holding)
+                {
+                    if (holdLocked)
+                    {
                         scaleUp = StartCoroutine(ScaleUpFeald());
-                    }else{
+                    }
+                    else
+                    {
                         scaleDown = StartCoroutine(ScaleDownFeald());
                     }
                 }
                 hasChecked = true;
-            }else{
+            }
+            else
+            {
                 hasChecked = false;
             }
         }
     }
-    public void HoldActive(InputAction.CallbackContext context){
-        if(transform == controller.GetActiveCharicter()){
-            if(context.performed){
+    public void HoldActive(InputAction.CallbackContext context)
+    {
+        if (transform == controller.GetActiveCharicter())
+        {
+            if (context.performed)
+            {
                 print("ok");
                 scaleUp = StartCoroutine(ScaleUpFeald());
-            }else if(!holdLocked){
+            }
+            else if (!holdLocked)
+            {
                 scaleDown = StartCoroutine(ScaleDownFeald());
             }
         }
     }
-        
-    private IEnumerator ScaleUpFeald() {
+
+    private IEnumerator ScaleUpFeald()
+    {
         if (scaleDown != null)
         {
             StopCoroutine(scaleDown);
         }
         gravFeald.gameObject.SetActive(true);
-        while (fealdScale < fealdScaleMax){
+        while (fealdScale < fealdScaleMax)
+        {
             yield return new WaitForSeconds(0.01f);
             fealdScale += fealdScale / scaleMod;
-            if (fealdScale > fealdScaleMax){ fealdScale = fealdScaleMax; }
+            if (fealdScale > fealdScaleMax) { fealdScale = fealdScaleMax; }
             gravFeald.transform.localScale = new Vector3(fealdScale, fealdScale, fealdScale);
         }
-     }
-    private IEnumerator ScaleDownFeald() {
-        if(scaleUp != null){
+    }
+    private IEnumerator ScaleDownFeald()
+    {
+        if (scaleUp != null)
+        {
             StopCoroutine(scaleUp);
         }
-        while (fealdScale > fealdScaleBase) {
+        while (fealdScale > fealdScaleBase)
+        {
             yield return new WaitForSeconds(0.01f);
             fealdScale -= fealdScale / scaleMod;
-            if (fealdScale < fealdScaleBase){ fealdScale = fealdScaleBase; }
-            gravFeald .transform.localScale = new Vector3(fealdScale, fealdScale, fealdScale);
+            if (fealdScale < fealdScaleBase) { fealdScale = fealdScaleBase; }
+            gravFeald.transform.localScale = new Vector3(fealdScale, fealdScale, fealdScale);
         }
         gravFeald.gameObject.SetActive(false);
+    }
+    private IEnumerator OrbBack(Rigidbody rb)
+    {
+        if(!orbReturning){
+            orbReturning = true;
+            Vector3 orbHoldWorld = transform.TransformPoint(orbHold);
+            //while (Vector3.Distance(orbBody.transform.position, orbHoldWorld) >1){
+                yield return new WaitForSeconds(0.01f);
+            //    rb.MovePosition(orbHoldWorld * 0.001f);
+            //}
+            gravOrb.transform.SetParent(camra);
+            //gravOrb.transform.localPosition = orbHold;
+            orbReturning = false;
+        }
      }
 }
